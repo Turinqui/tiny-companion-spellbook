dd075fc67d64ad7b1195a5676ceb14c91b8c4701
Turinqui <40512934+Turinqui@users.noreply.github.com>
2026-08-15 08:08:49 +1000

    Refresh cache for North Star update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tiny-companion-v7';
+const CACHE='tiny-companion-v8';
 const ASSETS=['./','./index.html','./styles.css','./theme.css','./app.js','./theme.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
